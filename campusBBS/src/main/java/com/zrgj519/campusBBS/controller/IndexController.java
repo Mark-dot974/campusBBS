589a285d6a7c54fe70125b30e3432332654be7f5
Sorrymaker0 <947111187@qq.com>
2022-07-11 22:46:27 +0800

    个人主页的获赞数、个性签名、setting、私信按钮

--- campusBBS/src/main/java/com/zrgj519/campusBBS/controller/IndexController.java
+++ campusBBS/src/main/java/com/zrgj519/campusBBS/controller/IndexController.java
@@ -76,7 +76,16 @@ public class IndexController {
         return "/site/404error";
     }
 
-    // 首页不分页
+    @RequestMapping("/collaborate-detail")
+    public String getCollaborateDetail() {
+        return "/site/collaborate-detail";
+    }
+
+    @RequestMapping("/collaborate")
+    public String getCollaborate() {
+        return "/site/collaborate";
+    }
+
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getPosts(Model model, Page page) {
         // 获取推荐内容
@@ -85,50 +94,11 @@ public class IndexController {
 
         // 根据用户是否登录，来获取到不同的内容
         User user = userContainer.getUser();
-        List<Post> posts = new ArrayList<>();
-        // 用户未登录
-        if(user == null)
-        {
-            posts = postService.getAllPosts();
-        }
-        // 登录
-        else{
-            String userInterest = user.getInterest();
-            // 用户没有选标签
-            if(userInterest == null || userInterest.equals("")){
-                posts = postService.getAllPosts();
-            }
-            // 根据用户兴趣查找出来的内容
-            else{
-                // 使用es根据用户兴趣查找内容
-                org.springframework.data.domain.Page<Post> postsByUserInterest
-                        = postService.findPostsByUserInterest(userInterest, page.getOffset(), page.getLimit());
-                List<Post> allPost = postService.getAllPosts();
-                // 根据用户兴趣查找不到帖子
-                if(postsByUserInterest == null){
-                    posts = allPost;
-                }
-                // 有帖子
-                else{
-                    int i = 0;
-                    int allPostsIndex = 0;
-                    // 把es中查到的内容取出来放到list中,3条感兴趣的，1条allPosts中的。
-                    for (Post post : postsByUserInterest) {
-                        i++;
-                        posts.add(post);
-                        // 避免重复
-                        allPost.remove(post);
-                        if(i%3==0){
-                            posts.add(allPost.remove(allPostsIndex++));
-                        }
-                    }
-                    posts.addAll(allPost);
-                }
-            }
-        }
-        // 封装用户信息
+
+        List<Post> allPosts = postService.getAllPosts();
         List<Map<String,Object>> postsInfo = new ArrayList<>();
-        for (Post post : posts) {
+        // 封装用户信息
+        for (Post post : allPosts) {
             Map<String,Object> postInfo = new HashMap<>();
             User userById = userService.findUserById(post.getUserId());
             postInfo.put("publisher",userById);
@@ -140,7 +110,25 @@ public class IndexController {
             }
             postsInfo.add(postInfo);
         }
+//        // 未登录
+//        if(user == null){
+//       }
+//        // 登录
+//        else{
+//            // 使用后面的搜索功能查询到对应的帖子
+//        }
         model.addAttribute("postsInfo", postsInfo);
         return "/site/index";
     }
+
+    @RequestMapping("/personal_page")
+    public String getPage(){
+        return "/site/personal_page";
+    }
+
+    @RequestMapping("/personal_post")
+    public String getPosts(){
+        return "/site/personal_post";
+    }
+
 }
