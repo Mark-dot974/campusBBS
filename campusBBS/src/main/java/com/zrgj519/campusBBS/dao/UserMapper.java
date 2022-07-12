d8867e6c9356eba952026bdc3fce9459905a2bb6
lyanchu <yanchu1114@163.com>
2022-07-12 11:17:05 +0800

    重新拉取并新增圈子管理

--- campusBBS/src/main/java/com/zrgj519/campusBBS/dao/UserMapper.java
+++ campusBBS/src/main/java/com/zrgj519/campusBBS/dao/UserMapper.java
@@ -37,8 +37,8 @@ public interface UserMapper {
 //    修改用户信息
     void updateUser(User user);
 
-    List<User> findUser(Integer id,String username,String email,Integer offset,Integer limit);
+    List<User> findUser(Integer id,String username,String email);
 
-    Integer selectUsersCount();
+
 
 }
