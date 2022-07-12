ab265bdde354c99a6b299892263d6d65eb3f3698
zinkt <zinkt@foxmail.com>
2022-07-12 15:42:38 +0800

    能部署到服务器，完成基本功能

--- campusBBS/src/main/java/com/zrgj519/campusBBS/controller/PostController.java
+++ campusBBS/src/main/java/com/zrgj519/campusBBS/controller/PostController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.WebParam;
 import java.util.*;
 
 @Controller
