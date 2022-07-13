7637c8b8402727e4db87826bcfb5650d0010321b
mark <2449333189@qq.com>
2022-07-13 14:26:30 +0800

    2022-7-13

--- campusBBS/src/main/java/com/zrgj519/campusBBS/dao/GroupMapper.java
+++ campusBBS/src/main/java/com/zrgj519/campusBBS/dao/GroupMapper.java
@@ -2,14 +2,13 @@ package com.zrgj519.campusBBS.dao;
 
 import com.zrgj519.campusBBS.entity.Group;
 import com.zrgj519.campusBBS.entity.GroupFile;
-import com.zrgj519.campusBBS.entity.Post;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface GroupMapper {
-    List<Group> selectAll();
+    List<Group> selectAll(int offset,int limit,String leaderName,String memberName);
 
     void insertGroup(Group group);
 
@@ -19,12 +18,5 @@ public interface GroupMapper {
 
     Group selectGroupById(int gid);
 
-
-    int deleteGroup(Integer gid);
-
-    Group find(Integer gid);
-
-    void updateGroup(Group group);
-
-    List<Group> findGroup(Integer gid,String groupName,String members);
+    void updateGroupMember(String members,int gid,int membersCount);
 }
