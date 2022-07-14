fde8c1ab1992b3d38a994e8278b51d12818844cb
mark <2449333189@qq.com>
2022-07-14 09:59:34 +0800

    2022-7-14 评论

--- campusBBS/src/main/resources/static/js/group.js
+++ campusBBS/src/main/resources/static/js/group.js
@@ -1,5 +1,33 @@
+function uploadFile(){
+    alert("happy")
+    $.ajax({
+        url: "http://up-z2.qiniup.com",
+        method: "post",
+        processData: false,
+        contentType: false,
+        data: new FormData($("#files")[0]),
+        success: function(data) {
+            $.post(
+                CONTEXT_PATH+"/group/uploadFile",
+                {"fileName":$("input[name='key']").val(),"gid":$("input[name='gid']").val()},
+                function(data) {
+                    data = $.parseJSON(data);
+                    if(data.code == 0) {
+                        window.location.reload();
+                    } else {
+                        alert(data.msg);
+                    }
+                })
+        }
+    });
+    // 表示不要将表单提交
+    return false;
+}
+
+
 function apply(){
     alert("apply")
+    sessionStorage.setItem("apply","apply");
     $.post(
         CONTEXT_PATH+"/group/apply",
         $('#applyForm').serialize(),
@@ -11,4 +39,70 @@ function apply(){
             }
         }
     )
+}
+
+function agree(){
+    sessionStorage.setItem("apply","agree");
+    $.post(
+        CONTEXT_PATH+"/group/operate",
+        $('#accept').serialize(),
+        function (data){
+            data = $.parseJSON(data);
+            if(data.code == 0)
+            {
+                window.location.reload();
+            }
+        }
+    )
+}
+
+function denied(){
+    sessionStorage.setItem("apply","denied");
+    $.post(
+        CONTEXT_PATH+"/group/operate",
+        $('#denied').serialize(),
+        function (data){
+            data = $.parseJSON(data);
+            if(data.code == 0)
+            {
+                window.location.reload();
+            }
+        }
+    )
+}
+
+function inviteMember(){
+    alert("invite");
+    $.post(
+        CONTEXT_PATH+"/group/invite",
+        $('#inviteForm').serialize(),
+        function (data){
+            data = $.parseJSON(data);
+            if(data.code == 0)
+            {
+                window.location.reload();
+            }else {
+                alert(data.msg);
+            }
+        }
+    )
+}
+
+
+$(function(){
+    if(sessionStorage.getItem("apply") == "agree") {
+        $('#apply').text("已同意");
+        $('#hi').html("");
+        sessionStorage.clear();
+    }
+    if(sessionStorage.getItem("apply") == "denied") {
+        $('#denied_text').text("已拒绝");
+        $('#hid').html("<div style='width: 80px'></div>");
+        sessionStorage.clear();
+    }
+
+})
+
+function memberForm(){
+    $('#invite').modal();
 }
